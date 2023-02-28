3b15c15ea6019f1fb44e6b99314aa3e554c0f286
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-02-27 22:21:06 -0500

    Create Run.sh

---
+++ Run.sh
@@ -0,0 +1,6 @@
+mkdir files
+python select_organisms.py seq_itgdb_seq.fasta Selected.txt files/selected.fasta
+apps/muscle -in files/selected.fasta -out files/msa.fasta
+apps/muscle -in files/msa.fasta -out files/refined.phylip -refine -phyi
+apps/phyml -i files/refined.phylip -m JC69 -o tlr
+python plot.py files/refined.phylip_phyml_tree.txt files/refined.phylip results.jpg
