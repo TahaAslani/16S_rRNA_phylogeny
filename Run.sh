3eef2032b7f0d6af4b735da6f76529484277138a
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-02-28 15:22:29 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -2,21 +2,30 @@ DB=$1
 Selected=$2
 result=$3
 
-echo "Generate phylogenic tree form 16S ribosomal RNA"
+echo "GENERATE PHYLOGENIC TREE FORM 16S RIBOSOMAL RNA"
+echo ""
 echo "credit: Taha Aslani"
 echo "https://github.com/TahaAslani/16S_rRNA_phylogeny"
+echo ""
 
 mkdir $result -p
 
+echo ""
 echo "Seperate the DNA sequqnce of the selected organisms..."
 python select_organisms.py $DB $Selected $result/selected.fasta
 
-echo "Alsign sequqnces..."
+echo ""
+echo "Allign sequqnces using Muscle..."
+
+echo "Step 1..."
 apps/muscle -in $result/selected.fasta -out $result/msa.fasta -quiet
+
+echo "Step 2..."
 apps/muscle -in $result/msa.fasta -out $result/refined.phylip -refine -phyi -quiet
 
-echo "Build the tree..."
+echo ""
+echo "Build the treeusing PhyML ..."
 apps/phyml -i $result/refined.phylip -m JC69 -o tlr --quiet
 
-echo "Plot"
+echo "Plot..."
 python plot.py $result/refined.phylip_phyml_tree.txt $result/results.jpg
