6d72159da20b3cee38e18ba94524b9f529f99c66
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-02-28 15:09:59 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -8,8 +8,8 @@ mkdir $result -p
 python select_organisms.py $DB $Selected $result/selected.fasta
 
 # Alsign sequqnces
-apps/muscle -in $result/selected.fasta -out $result/msa.fasta
-apps/muscle -in $result/msa.fasta -out $result/refined.phylip -refine -phyi
+apps/muscle -in $result/selected.fasta -out $result/msa.fasta -quiet
+apps/muscle -in $result/msa.fasta -out $result/refined.phylip -refine -phyi -quiet
 
 # Build the tree
 apps/phyml -i $result/refined.phylip -m JC69 -o tlr
