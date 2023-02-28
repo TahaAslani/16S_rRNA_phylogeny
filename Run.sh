5821a8ca6b9a3f1bc733e576445360c7781ffa5e
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2023-02-28 15:10:40 -0500

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -12,7 +12,7 @@ apps/muscle -in $result/selected.fasta -out $result/msa.fasta -quiet
 apps/muscle -in $result/msa.fasta -out $result/refined.phylip -refine -phyi -quiet
 
 # Build the tree
-apps/phyml -i $result/refined.phylip -m JC69 -o tlr
+apps/phyml -i $result/refined.phylip -m JC69 -o tlr --quiet
 
 # Plot
 python plot.py $result/refined.phylip_phyml_tree.txt $result/results.jpg
